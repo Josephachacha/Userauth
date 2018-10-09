b2e0c23e95fc8baf3edbf7e413836d857a514712
Josephachacha <odekachacha@gmail.com>
2018-10-09 18:55:23 +0300

    Authentication app complete

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^login/$', views.login_user, name='login'),
     url(r'^logout/$', views.logout_user, name='logout'),
+    url(r'^register/$', views.register_user, name='register'),
+    url(r'^edit_profile/$', views.edit_profile, name='edit_profile'),
+    url(r'^change_password/$', views.change_password, name='change_password'),
+
 
 
 ]
--- authenticate/views.py
+++ authenticate/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
+from  django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from .forms import SignUpForm, EditProfileForm
+
+
 
 def home(request):
     return render (request, 'authenticate/home.html', {})
@@ -31,3 +35,49 @@ def logout_user(request):
     logout(request)
     messages.success(request,('You have loged out!'))
     return redirect ('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Regisraion Successful!'))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    conext = {'form': form}
+    return render(request, 'authenticate/register.html', conext)
+
+
+def edit_profile(request):
+        if request.method == 'POST':
+            form = EditProfileForm(request.POST, instance = request.user)
+            if form.is_valid():
+                form.save()
+                messages.success(request, ('You have Updated your profile Successfully!'))
+                return redirect('home')
+        else:
+            form = EditProfileForm(instance = request.user)
+
+        conext = {'form': form}
+        return render(request, 'authenticate/edit_profile.html', conext)
+
+
+def change_password(request):
+        if request.method == 'POST':
+            form = PasswordChangeForm(data = request.POST, user = request.user)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, form.user)
+                messages.success(request, ('You have Changed Your Password!'))
+                return redirect('home')
+        else:
+            form = PasswordChangeForm(user = request.user)
+
+        conext = {'form': form}
+        return render(request, 'authenticate/change_password.html', conext)
